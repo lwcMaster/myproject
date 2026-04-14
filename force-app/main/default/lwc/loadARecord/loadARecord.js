3c1054602e2377978d3d6177fb3478e6b6f31234
Imran Khan <developer.ik@outlook.com>
2026-04-14 11:31:29 +0530

    Variable added

--- force-app/main/default/lwc/loadARecord/loadARecord.js
+++ force-app/main/default/lwc/loadARecord/loadARecord.js
@@ -5,4 +5,5 @@ export default class LoadARecord extends LightningElement {
     @api recordId;
     objectApiName ='Case';
     fields=[ 'CaseNumber', 'Priority','Status','Reason'];
+    caseIds;
 }
